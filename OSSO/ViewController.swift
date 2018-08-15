dae4b7a9dfb7edbf666e7f3689bf5c7b06ec5430
Joe Bakalor <jbak688@joes-macbook-pro.local>
2018-08-15 08:19:04 -0400

    Initial Commit

---
+++ OSSO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSSO
+//
+//  Created by Joe Bakalor on 8/15/18.
+//  Copyright © 2018 Joe Bakalor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
